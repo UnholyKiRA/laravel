e530a29bcb66cf9812e939681cf816d95eccbdb0
Gaming_With_KiRA <abhichigo121@gmail.com>
2022-11-25 18:48:23 +0900

    2022/11/25

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\About;
+use App\Models\MultiImage;
+use Illuminate\Support\Carbon;
 use Image;
 
 class AboutController extends Controller
@@ -59,5 +61,37 @@ class AboutController extends Controller
     public function HomeAbout(){
         $aboutpage =About::find(1);
         return view('frontend.about_page', compact('aboutpage'));
+    }
+
+    public function AboutMultiImage(){
+        return view('admin.about_page.multimage');
+    }
+
+    public function StoreMultiImage(Request $request){
+        $image= $request->file('multi_image');
+        foreach($image as $multi_image){
+
+            $name_gen = hexdec(uniqid()).'.'.$multi_image->getClientOriginalExtension();  // 3434343443.jpg
+
+            Image::make($multi_image)->resize(220,220)->save('upload/multi/'.$name_gen);
+            $save_url = 'upload/multi/'.$name_gen;
+
+            MultiImage::insert([
+                'multi_image' => $save_url,
+                'created_at' => Carbon::now()
+
+            ]); 
+        }
+            $notification = array(
+            'message' => 'Multi Image Successfully', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    }
+    public function AllMultiImage(){
+        $allMultiImage= MultiImage::all();
+        return view('admin.about_page.all_multiimage',compact('allMultiImage'));
     }
 }
