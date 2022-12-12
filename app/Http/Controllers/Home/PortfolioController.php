19370706e15bae4465e13fe047d76355265e421e
Gaming_With_KiRA <abhichigo121@gmail.com>
2022-12-12 18:58:58 +0900

    2022/12/12

--- app/Http/Controllers/Home/PortfolioController.php
+++ app/Http/Controllers/Home/PortfolioController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Portfolio;
 use Image;
+use Illuminate\Support\Carbon;
+
 
 class PortfolioController extends Controller
 {
@@ -33,26 +35,80 @@ class PortfolioController extends Controller
             'portfolio_title.required' => 'Portfolio Title is Required',
         ]);
         
-            $image = $request->file('home_slide');
+            $image = $request->file('portfolio_image');
             $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();  // 3434343443.jpg
 
-            Image::make($image)->resize(636,852)->save('upload/home_slide/'.$name_gen);
-            $save_url = 'upload/home_slide/'.$name_gen;
+            Image::make($image)->resize(1020,519)->save('upload/portfolio/'.$name_gen);
+            $save_url = 'upload/portfolio/'.$name_gen;
 
-            Portfolio::findOrFail($slide_id)->update([
-                'title' => $request->title,
-                'short_title' => $request->short_title,
-                'video_url' => $request->video_url,
-                'home_slide' => $save_url,
+            Portfolio::insert([
+                'portfolio_name' => $request->portfolio_name,
+                'portfolio_title' => $request->portfolio_title,
+                'portfolio_description' => $request->portfolio_description,
+                'portfolio_image' => $save_url,
+                'created_at' => Carbon::now(),
 
             ]); 
             $notification = array(
-            'message' => 'Home Slide Updated with Image Successfully', 
+            'message' => 'Portfolio Inserted Image Successfully', 
             'alert-type' => 'success'
         );
 
-        return redirect()->back()->with($notification);
+        return redirect()->route('all.portfolio')->with($notification);
 
     }//End Method
+
+    public function EditPortfolio($id){
+
+        $portfolio = Portfolio::findOrFail($id);
+        return view('admin.portfolio.portfolio_edit',compact('portfolio'));
+    }// End Method
+
+
+   public function UpdatePortfolio(Request $request){
+
+        $portfolio_id = $request->id;
+
+        if ($request->file('portfolio_image')) {
+            $image = $request->file('portfolio_image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();  // 3434343443.jpg
+
+            Image::make($image)->resize(1020,519)->save('upload/portfolio/'.$name_gen);
+            $save_url = 'upload/portfolio/'.$name_gen;
+
+            Portfolio::findOrFail($portfolio_id)->update([
+                'portfolio_name' => $request->portfolio_name,
+                'portfolio_title' => $request->portfolio_title,
+                'portfolio_description' => $request->portfolio_description,
+                'portfolio_image' => $save_url,
+
+            ]); 
+            $notification = array(
+            'message' => 'Portfolio Updated with Image Successfully', 
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.portfolio')->with($notification);
+
+        } else{
+
+            Portfolio::findOrFail($portfolio_id)->update([
+                'portfolio_name' => $request->portfolio_name,
+                'portfolio_title' => $request->portfolio_title,
+                'portfolio_description' => $request->portfolio_description,
+
+
+            ]); 
+            $notification = array(
+            'message' => 'Portfolio Updated without Image Successfully', 
+            'alert-type' => 'success'
+        );
+
+       return redirect()->route('all.portfolio')->with($notification);
+
+        } // end Else
+
+     } // End Method 
+
 
 }
--- resources/views/admin/portfolio/portfolio_edit.blade.php
+++ resources/views/admin/portfolio/portfolio_edit.blade.php
@@ -10,17 +10,17 @@
     <div class="card">
         <div class="card-body">
 
-            <h4 class="card-title">Protfolio Page </h4>
+            <h4 class="card-title">Protfolio Edit Page </h4>
             
-            <form method="post" action="{{ route('store.protfolio') }}" enctype="multipart/form-data">
+            <form method="post" action="{{ route('update.portfolio') }}" enctype="multipart/form-data">
                 @csrf
 
-               
+               <input type="hidden" name="id" value="{{  $portfolio->id }}">
 
             <div class="row mb-3">
                 <label for="example-text-input" class="col-sm-2 col-form-label">Protfolio Name</label>
                 <div class="col-sm-10">
-                    <input name="portfolio_name" class="form-control" type="text" id="example-text-input">
+                    <input name="portfolio_name" class="form-control" type="text" value="{{ $portfolio->portfolio_name }}" id="example-text-input">
                     @error('portfolio_name')
                     <span class="text-danger"> {{ $message }} </span>
                     @enderror
@@ -31,7 +31,7 @@
               <div class="row mb-3">
                 <label for="example-text-input" class="col-sm-2 col-form-label">Protfolio Title </label>
                 <div class="col-sm-10">
-                    <input name="portfolio_title" class="form-control" type="text" id="example-text-input">
+                    <input name="portfolio_title" class="form-control" type="text" value="{{ $portfolio->portfolio_title }}" id="example-text-input">
 
                     @error('portfolio_title')
                     <span class="text-danger"> {{ $message }} </span>
@@ -46,7 +46,7 @@
                 <label for="example-text-input" class="col-sm-2 col-form-label">Protfolio Description </label>
                 <div class="col-sm-10">
       <textarea id="elm1" name="portfolio_description">
-   
+      {{ $portfolio->portfolio_description }}
       </textarea>
                 </div>
             </div>
@@ -64,11 +64,11 @@
               <div class="row mb-3">
                  <label for="example-text-input" class="col-sm-2 col-form-label">  </label>
                 <div class="col-sm-10">
-  <img id="showImage" class="rounded avatar-lg" src="{{ url('upload/no_image.jpg') }}" alt="Card image cap">
+  <img id="showImage" class="rounded avatar-lg" src="{{ asset($portfolio->portfolio_image) }}" alt="Card image cap">
                 </div>
             </div>
             <!-- end row -->
-<input type="submit" class="btn btn-info waves-effect waves-light" value="Insert Protfolio Data">
+<input type="submit" class="btn btn-info waves-effect waves-light" value="Update Protfolio Data">
             </form>
              
            
